83df9a78b9b7519de48e7df2e987d93b64117f9f
Duglas Apaza <duglasapazaa@gmail.com>
2017-09-21 02:00:46 -0400

    Update HeroeapiService.java

--- app/src/main/java/com/dotalist/dalviik/dota2heroes/heroeapi/HeroeapiService.java
+++ app/src/main/java/com/dotalist/dalviik/dota2heroes/heroeapi/HeroeapiService.java
@@ -5,10 +5,6 @@ import com.dotalist.dalviik.dota2heroes.model.HeroeRespuesta;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-/**
- * Created by dxr on 20-09-17.
- */
-
 public interface HeroeapiService {
     @GET("heroes.json")
     Call<HeroeRespuesta> obtenerListaHeroe();
